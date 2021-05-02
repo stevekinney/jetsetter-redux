d1f236bb0f4da31486e58d2075be2f254934ae88
Steve Kinney <hello@stevekinney.net>
2021-05-02 16:49:38 -0600

    Initial commit

---
+++ src/Application.js
@@ -0,0 +1,9 @@
+const Application = () => {
+  return (
+    <div className="Application">
+      <h1>Hello World</h1>
+    </div>
+  );
+};
+
+export default Application;
---
+++ src/store/reducer.js
@@ -0,0 +1,5 @@
+export const initialState = null;
+
+export const reducer = (state = initialState, action) => {
+  return state;
+};
