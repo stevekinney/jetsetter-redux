2bc6066ef5034e0037f49c21e49ab3ce70d5cf7a
Steve Kinney <hello@stevekinney.net>
2021-05-02 18:10:38 -0600

    Add the ability to toggle and remove an item

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,16 +1,24 @@
+import { useDispatch } from 'react-redux';
+import { itemRemoved, itemToggled } from '../store/actions';
+
 export const Item = ({ item = {} }) => {
+  const dispatch = useDispatch();
+
+  const remove = () => dispatch(itemRemoved(item.id));
+  const toggle = () => dispatch(itemToggled(item.id));
+
   return (
     <article className="Item">
       <label htmlFor={item.id}>
         <input
           type="checkbox"
           checked={item.packed}
-          onChange={() => {}}
+          onChange={toggle}
           id={'item-' + item.id + '-toggle'}
         />
         {item.value}
       </label>
-      <button className="Item-remove" onClick={() => {}}>
+      <button className="Item-remove" onClick={remove}>
         Remove
       </button>
     </article>
